40e40e79d56c8c2796db0b2977b4e533de0f3dd1
Dima Chuvardinsky <dima.chuvardinsky@wesoftyou.com>
2018-06-08 12:42:20 +0300

    add base admin for User and Advertiser

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Advertiser
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username',
+                    'role', 'team',
+                    'last_login', 'date_joined')
+
+
+@admin.register(Advertiser)
+class AdvertiserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'team',
+                    'manager', 'publisher',
+                    'date_joined')
