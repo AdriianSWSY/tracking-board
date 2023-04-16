68bff83e259705e2bb347e0d4e64c305a5431f2c
AdriianSvyshchWSY <adriian.svyshch@wesoftyou.com>
2023-04-16 23:31:51 +0300

    Add: user and board fixtures

--- backend/conftest.py
+++ backend/conftest.py
@@ -1,5 +1,12 @@
+# Starts your fixture name from `factory_` means that it must return an object created by Factory.
+
 import pytest
 from rest_framework.test import APIClient, APIRequestFactory
+
+from apps.board.models import Board
+from apps.board.tests.factories import BoardFactory
+from apps.users.models import User
+from apps.users.tests.factories import UserFactory
 
 
 @pytest.fixture
@@ -10,3 +17,16 @@ def request_factory() -> APIRequestFactory:
 @pytest.fixture
 def api_client() -> APIClient:
     return APIClient()
+
+
+@pytest.fixture
+def factory_user() -> User:
+    """Return a User instance created by UserFactory"""
+    return UserFactory()
+
+
+@pytest.fixture
+def factory_board(factory_user) -> Board:
+    """Return a Board instance created by BoardFactory"""
+    return BoardFactory(creator=factory_user)
+
